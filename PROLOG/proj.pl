191f1f2a233f2884788f707071a0cf8746a388c0
barteko <haldman123@gmail.com>
2025-06-10 00:45:30 +0200

    add prolog project

--- PROLOG/proj.pl
+++ PROLOG/proj.pl
@@ -3,27 +3,18 @@
 :- dynamic w/2.
 
 dodaj_obiekt(Nazwa, X, Y, Z, Szerokosc, Wysokosc) :-
+  \+ czy_istnieje(Nazwa),
+  \+ wspolrzedne(_, X, Y, Z).
   assertz(rozmiar(Nazwa, Szerokosc, Wysokosc)),
   assertz(w(Nazwa, pokoj)),
   assertz(wspolrzedne(Nazwa, X,Y,Z)),
-  \+ czy_kolizja_xyz(X,Y, Z, _).
+  \+ czy_kolizja_xyz(Nazwa, _).
+
+czy_istnieje(Obiekt) :-
+  w(Obiekt, pokoj).
 
 
-z_os(Obiekt, Z) :- w(Obiekt, NadrzednyObiekt), wspolrzedne(NadrzednyObiekt, _, _, Z).
 
-
-idz_gora() :-
-  przesun_obiekt(mysz, 0, 1, 0).
-idz_dol() :-
-  przesun_obiekt(mysz, 0, -1, 0).
-idz_prawo() :-
-  przesun_obiekt(mysz, 1, 0, 0).
-idz_lewo() :-
-  przesun_obiekt(mysz, -1, 0, 0).
-skocz() :-
-  przesun_obiekt(mysz, 0,0,1).
-zeskocz() :-
-  przesun_obiekt(mysz, 0,0,-1).
 
 przesun_obiekt(Obiekt, DX, DY, DZ) :-
     wspolrzedne(Obiekt, X0, Y0, Z0),
@@ -35,20 +26,6 @@ przesun_obiekt(Obiekt, DX, DY, DZ) :-
     assertz(wspolrzedne(Obiekt, X1, Y1, Z1)),
     czy_nie_wychodzi_za_pokoj(Obiekt).
 
-idz_do(Obiekt) :-
-    znajdz_sciezke(Obiekt, Sciezka),
-    przejdz_sciezke(Sciezka).
-
-
-znajdz_sciezke(X0, Y0,Z0, X1, Y1,Z1,  Sciezka) :-
-  bfs([[X0-Y0]], X1, Y1,Z0,  Odwrocona),
-  reverse(Odwrocona, Sciezka).
-
-znajdz_sciezke(Obiekt, Sciezka) :-
-  wspolrzedne(mysz, X0, Y0, Z0),
-  wspolrzedne(Obiekt, X1, Y1, Z1),
-  Z0 =:= Z1,
-  znajdz_sciezke(X0, Y0, Z0, X1, Y1, Z1, Sciezka).
 
 ruch(X, Y, NX, NY) :-
   member(DX, [-1, 0, 1]),
@@ -61,34 +38,8 @@ mozna_wejsc(X,Y, Z) :-
   \+ (czy_kolizja_xyz(X,Y,Z, Inny), Inny \= mysz),
   wspolrzedne(pokoj, XP, YP, _),
   rozmiar(pokoj, DX, DY),
-  X >= XP, X <= DX + XP,
+  X >= XP, X =< DX + XP,
   Y >= YP, Y < DY + YP.
-
-bfs([[X-Y | Sciezka] | _], X, Y, _, [X-Y | Sciezka]) :- !.  % warunek końca
-
-bfs([[X-Y | Sciezka] | Reszta], CX, CY, Z, Wynik) :-
-    findall([NX-NY, X-Y | Sciezka],
-        (
-            ruch(X, Y, NX, NY),
-            \+ member(NX-NY, [X-Y | Sciezka]),  % nie odwiedzaj ponownie
-            mozna_wejsc(NX, NY, Z)
-        ),
-        NoweSciezki),
-    append(Reszta, NoweSciezki, NowaKolejka),
-    bfs(NowaKolejka, CX, CY, Z, Wynik).
-
-
-
-przejdz_sciezke([]).
-przejdz_sciezke([NX-NY | Reszta]) :-
-  wspolrzedne(mysz, X, Y, _),
-  DX is NX -X ,
-  DY is NY - Y,
-  write(mysz_idzie_z(X:Y, do, NX:NY)), nl,
-  wykonaj_ruch(DX, DY),
-  przejdz_sciezke(Reszta).
-
-
 
 
 wykonaj_ruch(1,0) :- idz_prawo.
@@ -132,7 +83,7 @@ rozmiar(stolik, 3,3).
 rozmiar(drzwi, 9, 1).
 rozmiar(krzeszlo, 7,3).
 rozmiar(poduszka, 7, 3).
-rozmiar(czlowiek, 1, 1).
+rozmiar(mysz,1,1).
 
 wspolrzedne(pokoj, 0,0,0).
 wspolrzedne(lozko,22,20, 1).
@@ -146,18 +97,18 @@ wspolrzedne(szafa,0,0,0 ).
 wspolrzedne(podkladka,21,3,2 ).
 wspolrzedne(myszka,21,2,3 ).
 wspolrzedne(stolik,36,15,1 ).
-wspolrzedne(drzwi,6,0,0 ).
-wspolrzedne(krzeszlo,26,7,0 ).
-wspolrzedne(mysz, 7,0,0).
+wspolrzedne(drzwi,6,0,1 ).
+wspolrzedne(krzeszlo,26,7,1 ).
+wspolrzedne(mysz, 7,0,1).
 
 
 
-czy_kolizja_xyz(X,Y,Z, Obiekt) :-
-  wspolrzedne(Obiekt, X0, Y0, Z0),
-  rozmiar(Obiekt, DX, DY ),
-  X >= X0, X <= X0 + DX,
-  Y >= Y0, Y <= Y0 + DY,
-  Z0 =:= Z,
+czy_kolizja_xyz(Obiekt1, Obiekt2) :-
+  Obiekt1 \= Obiekt2,
+  czy_czesc_wspolna_xy(Obiekt1, Obiekt2),
+  wspolrzedne(Obiekt1, _,_,Z1),
+  wspolrzedne(Obiekt2, _,_,Z2),
+  Z1 =:= Z2,
   !.
 
 
@@ -168,8 +119,9 @@ czy_nie_wychodzi_za_pokoj(Obiekt1) :-
   wspolrzedne(Obiekt1, X2, Y2, _),
   rozmiar(pokoj, DX1, DY1),
   rozmiar(Obiekt1, DX2, DY2),
-  X2+DX2 <= X1+DX1, Y2 + DY2 <= Y1 + DY1,
-  X2 >= X1, Y2 >=Y1.
+  X2+DX2 =< X1+DX1, Y2 + DY2 =< Y1 + DY1,
+  X2 >= X1, Y2 >= Y1.
+
 
 
 
@@ -178,7 +130,7 @@ czy_czesc_wspolna_xy(Obiekt1, Obiekt2) :-
   wspolrzedne(Obiekt2, X2, Y2,_ ),
   rozmiar(Obiekt2, DX2, DY2),
   rozmiar(Obiekt1, DX1, DY1),
-  X1 + DX1 >= X2, X1 <= X2 + DX2, Y1 + DY1 >= Y2, Y1 <= Y2 +DY2.
+  X1 + DX1 >= X2, X1 =< X2 + DX2, Y1 + DY1 >= Y2, Y1 =< Y2 +DY2.
 
 
 czy_czesc_wspolna_y(Obiekt1, Obiekt2) :-
@@ -198,8 +150,6 @@ czy_czesc_wspolna_x(Obiekt1, Obiekt2) :-
     X2 < X1 + DX1.
 
 
-
-
 lezy_na(Obiekt1, Obiekt2) :-
     czy_czesc_wspolna_xy(Obiekt1, Obiekt2),
     wspolrzedne(Obiekt1, _,_, Z1),
@@ -216,7 +166,7 @@ pod(Obiekt1, Obiekt2) :-
     czy_czesc_wspolna_xy(Obiekt1, Obiekt2),
     wspolrzedne(Obiekt1, _,_, Z1),
     wspolrzedne(Obiekt2, _,_, Z2),
-    Z1 <= Z2.
+    Z1 < Z2.
 
 na_prawo(Obiekt1, Obiekt2) :-
     \+ czy_czesc_wspolna_xy(Obiekt1, Obiekt2),
@@ -254,6 +204,31 @@ pomiedzy(Obiekt1, Obiekt2, Obiekt3) :-
    ; (
      (na_dole(Obiekt1, Obiekt2), na_gorze(Obiekt1, Obiekt3));(na_dole(Obiekt1, Obiekt3); na_gorze(Obiekt1, Obiekt2))
    )).
+
+
+zakresy_sie_pokrywaja(P1, L1, P2, L2) :-
+    P1 < P2 + L2,
+    P2 < P1 + L1.
+
+
+obok(Obiekt1, Obiekt2) :-
+    wspolrzedne(Obiekt1, X1, Y1, _),
+    wspolrzedne(Obiekt2, X2, Y2, _),
+    rozmiar(Obiekt1, DX1, DY1),
+    rozmiar(Obiekt2, DX2, DY2),
+
+    (
+        zakresy_sie_pokrywaja(Y1, DY1, Y2, DY2),
+        (X1 + DX1 =:= X2 ; X2 + DX2 =:= X1)
+    ;
+        zakresy_sie_pokrywaja(X1, DX1, X2, DX2),
+        (Y1 + DY2 =:= Y2 ; Y2 + DY2 =:= Y1)
+    ).
+
+
+
+
+
 
 
 
